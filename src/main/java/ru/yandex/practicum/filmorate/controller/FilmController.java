b6c87aacae34d2a6d3dcb6a6535d5d1b339fc45f
Konstantin <kosovmaster@yandex.ru>
2024-02-24 01:55:49 +0300

    feat: ТЗ 10

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -11,7 +11,6 @@ import ru.yandex.practicum.filmorate.service.FilmServiceImpl;
 
 import javax.validation.Valid;
 import java.util.Collection;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/films")
